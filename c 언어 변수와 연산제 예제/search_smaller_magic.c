a2ebbf1bfb4066981450ad5cbf234ab26ac56f9c
vada42 <whgotn2387152014@gmail.com>
2025-08-27 20:23:53 +0900

    success

--- c 언어 변수와 연산제 예제/search_smaller_magic.c
+++ c 언어 변수와 연산제 예제/search_smaller_magic.c
@@ -4,20 +4,33 @@
 
 int main(){
 	int c; 
-	int array[5];
+	int array[5]={};
 	int i = 0;
 	for(i=0; i<6; i++){
 		c = getch();
 		array[i] =c;
 	}
+	search(array);
+	return 0;
 	
 }
 
-int search(arr, len){
-	int count =0;
-	while(count<10){
-		if (arr[count]<arr[count+1]){
-			array_sort[]
+int sort(int arr[]){
+		int count = 0;
+		int len = sizeof(arr);
+		int sort_arr[]={};
+		while(count<len){
+			if (arr[count] < arr[count+1]){
+				sort_arr[count] = arr[count];
+			}
+			else if (arr[count] > arr[count+1]){
+				sort_arr[count] = arr[count+1];
+			}
+			count++;
 		}
+		return sort_arr;
 	}
+	
+int search(int arr[]){
+	return sort(arr)[0];
 }
