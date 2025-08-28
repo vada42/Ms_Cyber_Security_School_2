1af24c42eb67f5561f9fdefa3cb6959081a0ae8d
vada42 <whgotn2387152014@gmail.com>
2025-08-28 16:59:04 +0900

    success

--- C_file/loop_array_magic.c
+++ C_file/loop_array_magic.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-	int arr[] = {1,2,3,4,5,6,7,8,9};
+	int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
 	int* p = arr;
 	int i;
 	printf("int arr[] = {");
-	for(i=0;i<9;i++){
+	for(i=0; i<14; i++){
 		if (i==8){
 			printf("%d}",p[i]);
 			break;
